04dc18fe3d8c0e4e2af001981a400e4be5b4d60a
welaskez <nazar.nikitenko.03@mail.ru>
2024-10-18 12:53:01 +0500

    feat: add user sc wrapper

--- wrappers/User.ts
+++ wrappers/User.ts
@@ -1,13 +1,29 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type UserConfig = {};
+export type UserConfig = {
+    depositTimestamp: bigint,
+    unlockTimestamp: bigint,
+    adminAddress: Address,
+    balance: bigint,
+    mainScAddress: Address,
+    rootMasterAddress: Address,
+    jettonWalletCode: Cell,
+};
 
 export function userConfigToCell(config: UserConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeUint(config.depositTimestamp, 32)
+        .storeUint(config.unlockTimestamp, 32)
+        .storeAddress(config.adminAddress)
+        .storeUint(config.balance, 64)
+        .storeAddress(config.mainScAddress)
+        .storeAddress(config.rootMasterAddress)
+        .storeRef(config.jettonWalletCode)
+        .endCell();
 }
 
 export class User implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) { }
 
     static createFromAddress(address: Address) {
         return new User(address);
@@ -24,6 +40,22 @@ export class User implements Contract {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
+        });
+    }
+
+    async sendWithdraw(
+        provider: ContractProvider,
+        via: Sender,
+        value: bigint,
+        queryId: bigint,
+    ) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(555, 32)
+                .storeUint(queryId, 64)
+                .endCell(),
         });
     }
 }
