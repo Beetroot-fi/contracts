692c7d9b58af896c221d5c5a5c0a116a245989ef
welaskez <nazar.nikitenko.03@mail.ru>
2024-10-19 13:05:48 +0500

    feat: add jetton wallet wrapper

--- wrappers/JettonWalletCommon.ts
+++ wrappers/JettonWalletCommon.ts
@@ -1,9 +1,18 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, toNano } from '@ton/core';
 
-export type JettonWalletCommonConfig = {};
+export type JettonWalletCommonConfig = {
+    ownerAddress: Address,
+    jettonMasterAddress: Address,
+    jettonWalletCode: Cell,
+};
 
 export function jettonWalletCommonConfigToCell(config: JettonWalletCommonConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeCoins(0)
+        .storeAddress(config.ownerAddress)
+        .storeAddress(config.jettonMasterAddress)
+        .storeRef(config.jettonWalletCode)
+        .endCell();
 }
 
 export class JettonWalletCommon implements Contract {
@@ -24,6 +33,31 @@ export class JettonWalletCommon implements Contract {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
+        });
+    }
+
+    async sendTransfer(provider: ContractProvider, via: Sender,
+        opts: {
+            value: bigint;
+            toAddress: Address;
+            queryId: number;
+            fwdAmount: bigint;
+            jettonAmount: bigint;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0xf8a7ea5, 32)
+                .storeUint(opts.queryId, 64)
+                .storeCoins(opts.jettonAmount)
+                .storeAddress(opts.toAddress)
+                .storeAddress(via.address)
+                .storeUint(0, 1)
+                .storeCoins(opts.fwdAmount)
+                .storeUint(0, 1)
+                .endCell(),
         });
     }
 }
