43f90c2e991436b0f8ca9d9513cb339b7dce8fbe
welaskez <nazar.nikitenko.03@mail.ru>
2024-10-23 22:28:54 +0500

    feat: add getters to main sc wrapper

--- wrappers/Main.ts
+++ wrappers/Main.ts
@@ -2,20 +2,20 @@ import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, 
 
 export type MainConfig = {
     usdtJettonMasterAddress: Address,
-    usdtJettonWalletCode: Cell,
     rootMasterAddress: Address,
     userScCode: Cell,
     adminAddress: Address,
+    usdtJettonWalletCode: Cell,
     jettonWalletCode: Cell,
 };
 
 export function mainConfigToCell(config: MainConfig): Cell {
     return beginCell()
         .storeAddress(config.usdtJettonMasterAddress)
-        .storeRef(config.usdtJettonWalletCode)
         .storeAddress(config.rootMasterAddress)
         .storeRef(config.userScCode)
         .storeAddress(config.adminAddress)
+        .storeRef(config.usdtJettonWalletCode)
         .storeRef(config.jettonWalletCode)
         .endCell();
 }
@@ -39,5 +39,35 @@ export class Main implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async getUserScAddress(provider: ContractProvider, ownerAddress: Address): Promise<Address> {
+        const result = (await provider.get('get_user_sc_address', [
+            {
+                type: "slice",
+                cell: beginCell().storeAddress(ownerAddress).endCell()
+            }
+        ])).stack
+
+        return result.readAddress();
+    }
+
+    async getData(provider: ContractProvider) {
+        const result = (await provider.get('get_main_data', [])).stack
+        let usdtJettonMasterAddress = result.readAddress();
+        let rootMasterAddress = result.readAddress();
+        let userScCode = result.readCell();
+        let adminAddress = result.readAddress();
+        let usdtJettonWalletCode = result.readCell();
+        let jettonWalletCode = result.readCell();
+
+        return {
+            usdtJettonMasterAddress,
+            rootMasterAddress,
+            userScCode,
+            adminAddress,
+            usdtJettonWalletCode,
+            jettonWalletCode,
+        }
     }
 }
