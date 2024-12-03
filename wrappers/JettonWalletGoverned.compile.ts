7c84465589870fd35e1f3315dd0a79dd7f664daa
welaskez <nazar.nikitenko.03@mail.ru>
2024-12-03 15:32:57 +0500

    refactor: dirs architecture

--- wrappers/JettonWalletGoverned.compile.ts
+++ wrappers/JettonWalletGoverned.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/jetton_wallet_governed.fc'],
+    targets: ['contracts/mocks/stablecoin/jetton_wallet_governed.fc'],
 };
